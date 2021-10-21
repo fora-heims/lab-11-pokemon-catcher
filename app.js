d9b06c7e4ffbe5046b3d1476674b639aa93f3fbb
fora-heims <foraheims@gmail.com>
2021-10-20 23:24:41 -0700

    Generate three random pokemon function created. Renders images on page load and button click. Added some style in CSS.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions and grab DOM elements
 import { pokemonArray } from './src/pokemon.js';
+import { encounterPokemon, capturePokemon } from './src/state-utils.js';
+import { findByPokemon, setPokedex, getPokedex } from './src/storage-utils.js';
 
 const radioZero = document.getElementById('pkmn-r1');
 const radioOne = document.getElementById('pkmn-r2');
@@ -10,8 +12,39 @@ const imgTwo = document.getElementById('pkmn-img-r3');
 const catchButton = document.getElementById('catch-button');
 
 // initialize global state
+let x = 0;
+let y = 0;
+let z = 0;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+function generateIndexes() {
+    x = Math.floor(Math.random() * pokemonArray.length);
+    y = Math.floor(Math.random() * pokemonArray.length);
+    z = Math.floor(Math.random() * pokemonArray.length);
+}
+
+function generatePokemon() {
+    generateIndexes();
+    while (x === y || x === z || y === z) {
+        generateIndexes();
+    }
+    imgZero.src = pokemonArray[x].url_image;
+    imgOne.src = pokemonArray[y].url_image;
+    imgTwo.src = pokemonArray[z].url_image;
+
+    getPokedex();
+
+    setPokedex();
+}
+
+// On Page Load:
+generatePokemon();
+
+// On Button Click
+catchButton.addEventListener('click', () => {
+    getPokedex();
+
+    setPokedex('obj');
+    generatePokemon();
+    console.log([x, y, z]);
+});
+
