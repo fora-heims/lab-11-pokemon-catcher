dda935af6b55b7418df2abe45ade60c25f71ca41
fora-heims <foraheims@gmail.com>
2021-10-24 11:30:12 -0700

    attached one chart, no data connection to the app yet. Completed stretch goal: As the user is playing, display captures and encounters for each pokemon below each pokemon image

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // import functions and grab DOM elements
 import { pokemonArray } from './src/pokemon.js';
-import { createInitialPokedex, encounterPokemon, capturePokemon } from './src/storage-utils.js';
+import { createInitialPokedex, encounterPokemon, capturePokemon, findByPokemon, getPokedex } from './src/storage-utils.js';
 
 const imgZero = document.getElementById('pkmn-img-r1');
 const imgOne = document.getElementById('pkmn-img-r2');
 const imgTwo = document.getElementById('pkmn-img-r3');
+const sZero = document.getElementById('pkmn-s1');
+const sOne = document.getElementById('pkmn-s2');
+const sTwo = document.getElementById('pkmn-s3');
+const ssZero = document.getElementById('pkmn-ss1');
+const ssOne = document.getElementById('pkmn-ss2');
+const ssTwo = document.getElementById('pkmn-ss3');
 const catchButton = document.getElementById('catch-button');
 
 // initialize global state
@@ -26,9 +32,22 @@ function generatePokemon() {
     while (x === y || x === z || y === z) {
         generateIndexes();
     }
+    let pokedex = getPokedex();
+    let xDex = findByPokemon(pokedex, pokemonArray[x].pokemon);
+    let yDex = findByPokemon(pokedex, pokemonArray[y].pokemon);
+    let zDex = findByPokemon(pokedex, pokemonArray[z].pokemon);
+
     imgZero.src = pokemonArray[x].url_image;
     imgOne.src = pokemonArray[y].url_image;
     imgTwo.src = pokemonArray[z].url_image;
+
+    sZero.textContent = `Appeared: ${xDex.appeared}`;
+    sOne.textContent = `Appeared: ${yDex.appeared}`;
+    sTwo.textContent = `Appeared: ${zDex.appeared}`;
+    
+    ssZero.textContent = `Caught: ${xDex.chosen}`;
+    ssOne.textContent = `Caught: ${yDex.chosen}`;
+    ssTwo.textContent = `Caught: ${zDex.chosen}`;
 
     randomArray = [x, y, z];
     encounterPokemon(randomArray);
